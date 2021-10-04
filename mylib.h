8a09970d9e075e81b1bcc90d8427b5dc8665ab86
mikael750 <91464417+mikael750@users.noreply.github.com>
2021-10-04 11:09:54 +0200

    Add files via upload

---
+++ mylib.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>  
+#include <dirent.h>
+#include <errno.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int search();
+
+int load();
