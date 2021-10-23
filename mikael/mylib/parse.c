12172a22cf425e126a911804fe1635741cc02c31
mikael750 <91464417+mikael750@users.noreply.github.com>
2021-10-23 15:11:43 +0200

    Update parse.c

--- mikael/mylib/parse.c
+++ mikael/mylib/parse.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "treelib.h"
 
-int parse(const char *src, char *v1, char *v2){
+int parse(const char *src, char *v1, char *v2){        //parse un fichier
         if (sscanf(src,"%[^= ] = %[^=]", v1, v2) == 2)
             return 1;
         else
