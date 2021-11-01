e0f2d2ff8c89e9fb78f40089059cf3f5fbf65412
mikael750 <91464417+mikael750@users.noreply.github.com>
2021-11-01 18:35:06 +0100

    Update search.c

--- treelib/search.c
+++ treelib/search.c
@@ -14,9 +14,30 @@
 
 //localise les occurrences d’une entrée donnée (dont le nom est fourni en argument) dans la structure de données précédente(load).
 
-int search(){
+int search(int argc, char *argv[]){
 
+   DIR *dp; //
+   struct dirent *dirp; // 
     
+   if (argc != 2) {
+       fprintf(stderr, "Usage: ./program directory_name\n");
+       exit(EXIT_FAILURE);
+   }
 
-    return 0;
+   dp = opendir(argv[1]); //ouvre le repertoire specifié sur la ligne de commande
+
+   if(!dp){
+
+     printf("répertoire introuvable ou manque de permission\n");
+     return 1;
+   }
+   
+   while ((dirp = readdir(dp)) != NULL){
+      printf("%s\n", dirp->d_name); //liste les entrées du répertoire spécifié sur la ligne de commande.
+   }
+
+   closedir(dp);
+   exit(EXIT_SUCCESS);
+
+   return 0;    
 }
